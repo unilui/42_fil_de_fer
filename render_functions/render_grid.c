802e9923b69c0e9ad5d8fb48c810bf31f462a99d
Felipe Camargo <lufelip2@student.42sp.org.br>
2022-07-23 10:03:15 -0300

    map info

--- render_functions/render_grid.c
+++ render_functions/render_grid.c
@@ -6,13 +6,40 @@
 /*   By: lufelip2 <lufelip2@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/23 05:57:22 by lufelip2          #+#    #+#             */
-/*   Updated: 2022/07/23 07:31:26 by lufelip2         ###   ########.fr       */
+/*   Updated: 2022/07/23 10:01:36 by lufelip2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../fdf.h"
 
+void	map_info(t_pixel **map, int *columns, int *rows)
+{
+	int	y;
+	int	x;
+
+	y = 0;
+	x = 0;
+	while(map[y])
+	{
+		x = 0;
+		while(map[y] + x)
+		{
+			if ((map[y] + x)->eol)
+				break ;
+			x++;
+		}
+		y++;
+	}
+	*rows = y;
+	*columns = ++x;
+}
+
 void	render_grid(t_screen *screen)
 {
+	int	columns;
+	int	rows;
+
+	map_info(screen->map, &columns, &rows);
+	printf("%d x %d\n", columns, rows);
 	mlx_loop(screen->mlx);
 }
