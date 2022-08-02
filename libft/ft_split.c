e646ea5472f4c69a03e4ea9bd1976a956a0bd2b8
lufelip2@student.42sp.org.br <lufelip2@student.42sp.org.br>
2022-08-03 00:21:46 +0200

    No more tears

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -3,90 +3,74 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lufelip2 <lufelip2@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: lufelip2 <lufelip2@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 15:32:01 by lufelip2          #+#    #+#             */
-/*   Updated: 2022/06/08 15:17:25 by lufelip2         ###   ########.fr       */
+/*   Updated: 2022/08/03 00:13:46 by lufelip2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	word_count(char const *s, char c);
-static char	*new_str(char const *s, int len);
-static int	next_break(char const *s, char c);
+static int	word_count(char const *str, char sep);
+static char	*new_word(char const *str, char sep);
 
-char	**ft_split(char const *s, char c)
+char	**ft_split(char const *str, char sep)
 {
-	char	**arr;
-	char	*str;
-	char	*start;
-	int		i;
+	char	**split_str;
 	int		words;
+	int		i;
 
 	i = 0;
-	str = ft_strtrim(s, &c);
-	start = str;
-	words = word_count(str, c);
-	arr = (char **) malloc((words + 1) * sizeof(char *));
-	if (arr == NULL)
+	words = word_count(str, sep);
+	split_str = malloc((words + 1) * sizeof(char *));
+	if (!split_str)
 		return (NULL);
 	while (i < words)
-	{
-		arr[i++] = new_str(str, next_break(str, c));
-		str += (next_break(str, c));
-		while (*str == c)
-			str++;
-	}
-	arr[i] = NULL;
-	free(start);
-	return (arr);
+		split_str[i++] = new_word(str, sep);
+	split_str[i] = NULL;
+	return (split_str);
 }
 
-static int	word_count(char const *s, char c)
+static int	word_count(char const *str, char sep)
 {
-	int	i;
 	int	words;
 
-	i = 0;
 	words = 1;
-	if (ft_strlen(s) == 0)
-		return (0);
-	while (s[i])
+	while (*str == sep)
+		str++;
+	while (*str)
 	{
-		if (s[i] == c && s[i - 1] != c)
+		if (*str == sep && (*(str + 1) != sep) && *(str + 1))
 			words++;
-		i++;
+		str++;
 	}
 	return (words);
 }
 
-static int	next_break(char const *s, char c)
+static char	*new_word(char const *str, char sep)
 {
-	int	i;
+	static int	offset;
+	int			letters;
+	int			reverse;
+	char		*word;
 
-	i = 0;
-	while (s[i])
+	letters = 0;
+	reverse = 1;
+	while (str[offset] == sep)
+		offset++;
+	while (str[offset] != sep && str[offset])
 	{
-		if (s[i] == c)
-			return (i);
-		i++;
+		letters++;
+		offset++;
 	}
-	return (i);
-}
-
-static char	*new_str(char const *s, int len)
-{
-	char	*str;
-	int		i;
-
-	i = 0;
-	str = malloc(len + 1);
-	while (i < len)
-	{
-		str[i] = s[i];
-		i++;
-	}
-	str[i] = '\0';
-	return (str);
+	word = malloc(letters + 1);
+	if (!word)
+		return (NULL);
+	word[letters--] = '\0';
+	while (letters >= 0)
+		word[letters--] = str[offset - reverse++];
+	if (!str[offset])
+		offset = 0;
+	return (word);
 }
